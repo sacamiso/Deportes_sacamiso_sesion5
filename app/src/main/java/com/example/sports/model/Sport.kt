3ddef29fb99a4ba954208739fbaefe144f873ed4
sacamiso <sacamiso@unirioja.es>
2023-03-03 16:11:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sports/model/Sport.kt
@@ -0,0 +1,14 @@
+package com.example.sports.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+
+data class Sport(
+    val id: Int,
+    @StringRes val titleResourceId: Int,
+    @StringRes val subtitleResourceId: Int,
+    @DrawableRes val imageResourceId: Int,
+    @DrawableRes val sportsImageBanner: Int,
+    @StringRes val newsDetails: Int
+)
